c80b94c83a9b5e058d1b8198b25633e5fa775558
martyewings <martyewings@gmail.com>
2021-11-10 15:44:42 +0000

    convert confinement to if

--- lib/facter/self_service.rb
+++ lib/facter/self_service.rb
@@ -25,43 +25,44 @@ Facter.add(:self_service, type: :aggregate) do
   end
 
   chunk(:S0003) do
-    confine is_pe: true
-    # check for noop logic flip as false is the desired state
-    result = Facter::Core::Execution.execute('puppet config print noop')
-    if result.include?('false')
-      { S0003: true }
-    else
-      { S0003: false }
+    if Facter.value(:is_pe)
+      # check for noop logic flip as false is the desired state
+      result = Facter::Core::Execution.execute('puppet config print noop')
+      if result.include?('false')
+        { S0003: true }
+      else
+        { S0003: false }
+      end
     end
   end
 
   chunk(:S0004) do
-    confine is_pe: true
-    # Check for status that is not green, potentially need a better way of doing this, or perhaps calling the api directly for each service
-    result = Facter::Core::Execution.execute('puppet infrastructure status')
-    if result.include?('Unknown') || result.include?('Unreachable')
-      { S0004: false }
-    else
-      { S0004: true }
+    if Facter.value(:is_pe)
+      # Check for status that is not green, potentially need a better way of doing this, or perhaps calling the api directly for each service
+      result = Facter::Core::Execution.execute('puppet infrastructure status')
+      if result.include?('Unknown') || result.include?('Unreachable')
+        { S0004: false }
+      else
+        { S0004: true }
+      end
     end
   end
 
   chunk(:S0005) do
-    #Check if the CA expires within 90 days confined to servers where the ca_cert exists
-    confine do
-      File.exist? '/etc/puppetlabs/puppet/ssl/ca/ca_crt.pem' or File.exist? '/etc/puppetlabs/puppetserver/ca/ca_crt.pem'
-    end
-    raw_ca_cert = if File.exist? '/etc/puppetlabs/puppetserver/ca/ca_crt.pem'
-                    File.read '/etc/puppetlabs/puppetserver/ca/ca_crt.pem'
-                  else
-                    File.read '/etc/puppetlabs/puppet/ssl/ca/ca_crt.pem'
-                  end
-    certificate = OpenSSL::X509::Certificate.new raw_ca_cert
-    result = certificate.not_after - Time.now
-    if result > 7_776_000
-      { S0005: true }
-    else
-      { S0005: false }
+    # Check if the CA expires within 90 days confined to servers where the ca_cert exists
+    if File.exist?('/etc/puppetlabs/puppet/ssl/ca/ca_crt.pem') || File.exist?('/etc/puppetlabs/puppetserver/ca/ca_crt.pem')
+      raw_ca_cert = if File.exist? '/etc/puppetlabs/puppetserver/ca/ca_crt.pem'
+                      File.read '/etc/puppetlabs/puppetserver/ca/ca_crt.pem'
+                    else
+                      File.read '/etc/puppetlabs/puppet/ssl/ca/ca_crt.pem'
+                    end
+      certificate = OpenSSL::X509::Certificate.new raw_ca_cert
+      result = certificate.not_after - Time.now
+      if result > 7_776_000
+        { S0005: true }
+      else
+        { S0005: false }
+      end
     end
   end
 end
