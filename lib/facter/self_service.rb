29eb09b26648d903802158996c1a432c1f9969a3
martyewings <martyewings@gmail.com>
2021-12-07 14:10:33 +0000

    Last Agent Run Health Checks

--- lib/facter/self_service.rb
+++ lib/facter/self_service.rb
@@ -1,5 +1,4 @@
 # Self service fact aims to have all chunks reporting as true, this indicates ideal state, any individual chunk reporting false should be alerted on and checked against documentation for next steps
-require 'puppet'
 # Use shared logic from PuppetSelfService
 require_relative '../shared/puppet_self_service'
 ### TODO now puppet is required, use puppet functions insead of execute shell blocks where possible
@@ -7,7 +6,8 @@ require_relative '../shared/puppet_self_service'
 Facter.add(:self_service, type: :aggregate) do
   confine kernel: 'Linux'
   confine { Facter.value(:pe_build) }
-
+  require 'puppet'
+  require 'yaml'
   puppet_bin = '/opt/puppetlabs/bin/puppet'
 
   chunk(:S0001) do
@@ -86,5 +86,22 @@ Facter.add(:self_service, type: :aggregate) do
     # Is the pe-postgres Service Running and Enabled
     postgresversion = PuppetSelfService.pe_postgres_service_name
     { S0011: PuppetSelfService.service_running_enabled(postgresversion.to_s) }
+  end
+
+  chunk(:S0012) do
+    next unless PuppetSelfService.replica? || PuppetSelfService.postgres? || PuppetSelfService.primary? || PuppetSelfService.compiler? || PuppetSelfService.legacy_compiler?
+    # Did Puppet Produce a report in the last run interval
+    summary_path = Puppet.settings['lastrunfile']
+    lastrunfile = YAML.load_file(summary_path)
+    time_lastrun = lastrunfile['time']['last_run']
+    since_lastrun = Time.now - time_lastrun
+    { S0012: since_lastrun.to_i <= Puppet.settings['runinterval'] }
+  end
+
+  chunk(:S0013) do
+    next unless PuppetSelfService.replica? || PuppetSelfService.postgres? || PuppetSelfService.primary? || PuppetSelfService.compiler? || PuppetSelfService.legacy_compiler?
+    # Did catalog apply successfully on last puppet run
+    summary_path = Puppet.settings['lastrunfile']
+    { S0013: File.open(summary_path).read.include?('catalog_application') }
   end
 end
