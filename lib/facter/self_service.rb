224cdd41fc0b460cb6160e1ea0b929eec4e20bbd
Bartosz Blizniak <bartosz.blizniak@puppet.com>
2022-01-20 10:13:21 +0000

    testing new change

--- lib/facter/self_service.rb
+++ lib/facter/self_service.rb
@@ -108,14 +108,10 @@ Facter.add(:self_service, type: :aggregate) do
     { S0013: File.open(summary_path).read.include?('catalog_application') }
   end
 
-  chunk(:S00014) do
+  chunk(:S0014) do
     file_time = File.mtime('/opt/puppetlabs/server/data/puppetdb/stockpile/cmd/q')
     time_now = Time.now - 1800
-    if time_now.to_i < file_time.to_i
-      { S00014: true }
-    else
-      { S00014: false }
-    end
+    { S0014: time_now.to_i < file_time.to_i }
   end
 
   chunk(:S0021) do
