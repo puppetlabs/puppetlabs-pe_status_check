f0aa5b703b128f98bd88394f0c4b64e2a6adce3d
Jarret Lavallee <jarret@puppet.com>
2022-01-27 09:06:20 -0700

    (fix) S0036 acceptance tests should use apply
    Prior to this commit, the acceptance tests for S0036 were setting hiera
    data and running the puppet agent. This caused intermittant failures due
    to timing issues. This commit changes the behavior to use a puppet apply
    block to set the setting.

--- spec/acceptance/self_service_spec.rb
+++ spec/acceptance/self_service_spec.rb
@@ -347,12 +347,25 @@ hierarchy:
 >>>>>>> (SUP-2919) - is puppet_metrics_collector::system configured
 =======
       it 'if S0036 conditions for false are met' do
-        run_shell('echo "puppet_enterprise::master::puppetserver::jruby_puppet_max_queued_requests: 151" >> /etc/puppetlabs/code/environments/production/data/common.yaml')
-        run_shell('puppet resource service puppet ensure=stopped')
-        run_shell('puppet agent -t', expect_failures: true)
+        present = <<-PUPPETCODE
+        pe_hocon_setting { 'jruby-puppet.max-queued-requests':
+          ensure  => present,
+          path    => '/etc/puppetlabs/puppetserver/conf.d/pe-puppet-server.conf',
+          setting => 'jruby-puppet.max-queued-requests',
+          value   => 151,
+        }
+        PUPPETCODE
+        absent = <<-PUPPETCODE
+        pe_hocon_setting { 'jruby-puppet.max-queued-requests':
+          ensure  => absent,
+          path    => '/etc/puppetlabs/puppetserver/conf.d/pe-puppet-server.conf',
+          setting => 'jruby-puppet.max-queued-requests',
+        }
+        PUPPETCODE
+        apply_manifest(present)
         result = run_shell('facter -p self_service.S0036')
         expect(result.stdout).to match(%r{false})
-        run_shell('puppet resource service puppet ensure=running')
+        apply_manifest(absent)
       end
 >>>>>>> SUP-2910 to check the Old PE package
       it 'if S0040 conditions for false are met' do
