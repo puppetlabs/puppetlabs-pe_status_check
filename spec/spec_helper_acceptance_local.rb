0a737c6d43e1cf67bc16222395453333ffd16bc5
paddy <patrick.grant@puppet.com>
2022-01-28 12:15:23 +0000

    test license update

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -23,6 +23,7 @@ RSpec.configure do |c|
     # Put a test PE license in place, set its ownership and permissions
     PuppetLitmus::PuppetHelpers.run_shell('echo -e "#######################\n#  Begin License File #\n#######################\n \n# PUPPET ENTERPRISE LICENSE - test
 <<<<<<< HEAD
+<<<<<<< HEAD
             \nuuid: test\n \nto: test\n \nnodes: 100\n \nlicense_type: Subscription\n \nsupport_type: PE Premium\n \nstart: 2022-01-01\n \nend: 2025-12-31
             \n#####################\n#  End License File #\n#####################" >> /etc/puppetlabs/license.key')
     PuppetLitmus::PuppetHelpers.run_shell('sudo chown root:root /etc/puppetlabs/license.key')
@@ -31,6 +32,9 @@ RSpec.configure do |c|
     PuppetLitmus::PuppetHelpers.run_shell('puppet resource service pe-puppetserver ensure=stopped; puppet resource service pe-puppetserver ensure=running')
 =======
             \nuuid: ****test****\n \nto: test\n \nnodes: 100\n \nlicense_type: Subscription\n \nsupport_type: PE Premium\n \nstart: 2022-01-01\n \nend: 2025-12-31
+=======
+            \nuuid: test\n \nto: test\n \nnodes: 100\n \nlicense_type: Subscription\n \nsupport_type: PE Premium\n \nstart: 2022-01-01\n \nend: 2025-12-31
+>>>>>>> test license update
             \n#####################\n#  End License File #\n#####################" >> /etc/puppetlabs/license.key')
     PuppetLitmus::PuppetHelpers.run_shell('sudo chown root:root /etc/puppetlabs/license.key')
     PuppetLitmus::PuppetHelpers.run_shell('sudo chmod 644 /etc/puppetlabs/license.key')
