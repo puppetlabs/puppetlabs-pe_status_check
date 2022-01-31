ac10dc980ab4883e18297daf3c85da55e0eb43d4
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2022-01-31 15:06:01 +0000

    (SUP-2945) Creating a shared function to call unauthenticated PE APIs

--- lib/facter/self_service.rb
+++ lib/facter/self_service.rb
@@ -7,7 +7,6 @@ Facter.add(:self_service, type: :aggregate) do
   require 'puppet'
   require 'yaml'
   require_relative '../shared/puppet_self_service'
-  puppet_bin = '/opt/puppetlabs/bin/puppet'
 
   chunk(:S0001) do
     # Is the Agent Service Running and Enabled
@@ -25,11 +24,14 @@ Facter.add(:self_service, type: :aggregate) do
   end
 
   chunk(:S0004) do
-    next unless PuppetSelfService.primary?
     # Is PE and has clienttools covers pe-psql and compilers
     # Check for service status that is not green, potentially need a better way of doing this, or perhaps calling the api directly for each service
-    result = Facter::Core::Execution.execute("#{puppet_bin} infrastructure status")
-    if result.include?('Unknown') || result.include?('Unreachable')
+
+    puppetendpoint = 'https://localhost:8140/status/v1/services'
+
+    puppetendpointcall = PuppetSelfService.nethttp_puppet_api(puppetendpoint)
+
+    if puppetendpointcall.include?('Unknown') || puppetendpointcall.include?('Unreachable')
       { S0004: false }
     else
       { S0004: true }
