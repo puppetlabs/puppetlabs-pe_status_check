315b6735acf4e36c07603c8c7df94ce164f6ead4
Selvakumar.subburaj <Selvakumar@puppet.com>
2022-01-31 18:28:44 +0800

    added rescue statement, fliped logic from true to false, updated the query to take only 30 minutes data

--- lib/facter/self_service.rb
+++ lib/facter/self_service.rb
@@ -192,15 +192,16 @@ Facter.add(:self_service, type: :aggregate) do
   chunk(:S0027) do
     next unless PuppetSelfService.primary?
     # Check if thundering herd is occuring.
-    thundering_query_output = PuppetSelfService.psql_thundering_herd.split(' ').drop(2).map(&:to_i)
-    thundering_query_output.delete(0)
-    thundering_occured = false
-    minvalue = thundering_query_output.min.to_i
-    maxvalue = thundering_query_output.max.to_i
-    differences = ((maxvalue - minvalue) * 100) / minvalue
-    if differences >= 75
-      thundering_occured = true
+    output_data = PuppetSelfService.psql_thundering_herd
+    if !output_data.nil?
+      thundering_query_output = output_data.split(' ').drop(2).map(&:to_i)
+      thundering_query_output.delete(0)
+      minvalue = thundering_query_output.min.to_i
+      maxvalue = thundering_query_output.max.to_i
+      differences = ((maxvalue - minvalue) * 100) / minvalue
+      { S0027: differences <= 75 }
+    else
+      { S0027: true }
     end
-    { S0027: thundering_occured }
   end
 end
