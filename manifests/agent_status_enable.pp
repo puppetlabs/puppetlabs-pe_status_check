c03532cc57bd435c7f0f09d144f36a624eca2df4
martyewings <martyewings@gmail.com>
2022-07-06 15:50:26 +0100

    (SUP-3450) Update File resource logic in manifest

--- manifests/agent_status_enable.pp
+++ manifests/agent_status_enable.pp
@@ -8,22 +8,18 @@
 # @param [Boolean] agent_status_enabled
 #  Flag to enable or disable agent_status_check fact
 class pe_status_check::agent_status_enable (
-  Boolean       $agent_status_enabled               = true,
+Boolean $agent_status_enabled = true,
 ){
   $agent_status_enabled_file = $facts['os']['family'] ? {
     'windows' => "${facts['common_appdata']}/PuppetLabs/puppet/cache/state/status_check_enable",
     default   => '/opt/puppetlabs/puppet/cache/state/status_check_enable',
   }
-if $agent_status_enabled {
-      file {
-        $agent_status_enabled_file:
-          ensure => file,
-          mode   => '0664',
-      }
-    } else {
-      file {
-        $agent_status_enabled_file:
-          ensure => absent,
-      }
-    }
-  }
+$agent_status_enabled_file_ensure = $agent_status_enabled ? {
+  true     => 'file',
+  default => 'absent',
+}
+file { $agent_status_enabled_file:
+  ensure => $agent_status_enabled_file_ensure,
+  mode   => '0664',
+}
+}
