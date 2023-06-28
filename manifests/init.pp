48eb1a434a25a8ce2fca1ad38989b8c155cda0a3
Dimitri Tischenko <dimitri@puppet.com>
2023-06-28 16:28:12 +0200

    Fix notify message, add spec test

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,7 +15,7 @@ class pe_status_check (
   $negatives.each |$indicator, $_v| {
     $in_message = lookup("pe_status_check.${indicator}", { default_value => 'Determines there is a fault' })
     notify { "pe_status_check ${indicator}":
-      message => "${indicator} is at fault. The indicator ${in_message}, refer to documentation for required action",
+      message => "${indicator} is at fault. The indicator ${indicator} ${in_message}, refer to documentation for required action",
     }
   }
 }
